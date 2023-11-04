67497923319816139fdea33a378b45ff591324a3
BartoszThielmann <bartoszthielmann@gmail.com>
2023-11-04 18:25:33 +0100

    Trim strings when data binding
    Registered custom editor in WebDataBinder which removes whitespaces
    when binding data from web requests to Java objects.

---
+++ src/main/java/com/bartoszthielmann/employeemanager/controller/InitBinderControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.bartoszthielmann.employeemanager.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class InitBinderControllerAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+}

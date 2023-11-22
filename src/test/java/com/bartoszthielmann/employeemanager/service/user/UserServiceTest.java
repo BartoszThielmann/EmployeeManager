8660d6af2a287fa31bf34dff28dbc08f0f867097
BartoszThielmann <bartoszthielmann@gmail.com>
2023-11-22 21:30:17 +0100

    Implement unit test cases for UserService.save
    *Added test cases for different username generation paths.
    *Minor refactoring of UserServiceTest to make use of @BeforeEach

--- src/test/java/com/bartoszthielmann/employeemanager/service/user/UserServiceTest.java
+++ src/test/java/com/bartoszthielmann/employeemanager/service/user/UserServiceTest.java
@@ -8,6 +8,7 @@ import com.bartoszthielmann.employeemanager.entity.UserDto;
 import com.bartoszthielmann.employeemanager.entity.UserInfo;
 import com.bartoszthielmann.employeemanager.service.UserService;
 import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,7 +21,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -36,9 +36,33 @@ public class UserServiceTest {
     private Validator validator;
     @Mock
     private PasswordEncoder passwordEncoder;
-
     @InjectMocks
     private UserService userService;
+
+    private UserDto userDto;
+    private String baseUsername;
+    private List<Role> roleList;
+
+
+    @BeforeEach
+    public void setup() {
+        userDto = new UserDto();
+        userDto.setId(0);
+        userDto.setFirstName("Joey");
+        userDto.setLastName("Salads");
+        userDto.setPassword("password123");
+        userDto.setRoles(new ArrayList<>(Arrays.asList("1", "2")));
+
+        baseUsername = "jsalads";
+
+        Role role1 = new Role();
+        role1.setId(1);
+        role1.setName("ROLE_ADMIN");
+        Role role2 = new Role();
+        role2.setId(2);
+        role2.setName("ROLE_USER");
+        roleList = new ArrayList<>(Arrays.asList(role1, role2));
+    }
 
     @Test
     public void test_createUserDtoFromUser() {
@@ -50,45 +74,22 @@ public class UserServiceTest {
         userInfo.setFirstName("Joey");
         userInfo.setLastName("Salads");
         user.setUserInfo(userInfo);
-        Role role1 = new Role();
-        role1.setId(1);
-        Role role2 = new Role();
-        role2.setId(2);
-        Set<Role> rolesSet = new HashSet<>();
-        rolesSet.add(role1);
-        rolesSet.add(role2);
-        user.setRoles(rolesSet);
+        user.setRoles(new HashSet<>(roleList));
 
         // when
-        UserDto userDto = userService.createUserDtoFromUser(user);
+        UserDto userDtoFromUser = userService.createUserDtoFromUser(user);
 
         // then
-        assertThat(userDto.getId()).isEqualTo(user.getId());
-        assertThat(userDto.getFirstName()).isEqualTo(user.getUserInfo().getFirstName());
-        assertThat(userDto.getLastName()).isEqualTo(user.getUserInfo().getLastName());
-        assertThat(userDto.getRoles()).contains(String.valueOf(role1.getId()), String.valueOf(role2.getId()));
+        assertThat(userDtoFromUser.getId()).isEqualTo(user.getId());
+        assertThat(userDtoFromUser.getFirstName()).isEqualTo(user.getUserInfo().getFirstName());
+        assertThat(userDtoFromUser.getLastName()).isEqualTo(user.getUserInfo().getLastName());
+        assertThat(userDtoFromUser.getRoles())
+                .contains(String.valueOf(roleList.get(0).getId()), String.valueOf(roleList.get(1).getId()));
     }
 
     @Test
     public void test_save_caseBaseUsername(){
         // given
-        UserDto userDto = new UserDto();
-        userDto.setId(0);
-        userDto.setFirstName("Joey");
-        userDto.setLastName("Salads");
-        userDto.setPassword("password123");
-        userDto.setRoles(new ArrayList<>(Arrays.asList("1", "2")));
-
-        String baseUsername = "jsalads";
-
-        Role role1 = new Role();
-        role1.setId(1);
-        role1.setName("ROLE_ADMIN");
-        Role role2 = new Role();
-        role2.setId(2);
-        role2.setName("ROLE_USER");
-        List<Role> roleList = new ArrayList<>(Arrays.asList(role1, role2));
-
         given(userDao.findUsernamesByPrefix(baseUsername)).willReturn(Collections.emptyList());
         given(userDao.findRolesByIds(Arrays.asList(1,2))).willReturn(roleList);
         given(passwordEncoder.encode(anyString())).willReturn("password");
@@ -105,5 +106,37 @@ public class UserServiceTest {
         assertThat(user.getUserInfo().getEmail()).isEqualTo(baseUsername + "@bth.com");
         assertThat(user.getUsername()).isEqualTo(baseUsername);
         assertThat(user.getPassword()).isEqualTo("password");
+    }
+
+    @Test
+    public void test_save_caseBaseUsername2(){
+        // given
+        given(userDao.findUsernamesByPrefix(baseUsername)).willReturn(Arrays.asList("jsalads"));
+        given(validator.validate(any())).willReturn(Collections.emptySet());
+        given(userDao.save(any(User.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        User user = userService.save(userDto);
+
+        // then
+        assertThat(user.getUserInfo().getFirstName()).isEqualTo(userDto.getFirstName());
+        assertThat(user.getUserInfo().getLastName()).isEqualTo(userDto.getLastName());
+        assertThat(user.getUsername()).isEqualTo(baseUsername + "2");
+    }
+
+    @Test
+    public void test_save_caseBaseUsernameWithMaxNumberPlusOne() {
+        // given
+        given(userDao.findUsernamesByPrefix(baseUsername)).willReturn(Arrays.asList("jsalads2"));
+        given(validator.validate(any())).willReturn(Collections.emptySet());
+        given(userDao.save(any(User.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        User user = userService.save(userDto);
+
+        // then
+        assertThat(user.getUserInfo().getFirstName()).isEqualTo(userDto.getFirstName());
+        assertThat(user.getUserInfo().getLastName()).isEqualTo(userDto.getLastName());
+        assertThat(user.getUsername()).isEqualTo(baseUsername + "3");
     }
 }
